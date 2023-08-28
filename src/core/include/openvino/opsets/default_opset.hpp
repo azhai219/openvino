20d05dfefcd66f501b4691a529dede69cf56d602
Xiuchuan Zhai <xiuchuan.zhai@intel.com>
2023-08-28 23:23:31 +0800

    help instance op from opset

---
+++ src/core/include/openvino/opsets/default_opset.hpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/opsets/opset12.hpp"
+
+namespace ov {
+namespace default_opset = ov::opset12;
+}   // namespace ov
